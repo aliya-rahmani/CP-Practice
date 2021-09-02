d934add59844f6b3349a4caeeb1a217b787f49a1
Aliya Rahmani <65673692+aliya-rahmani@users.noreply.github.com>
2021-09-02 12:39:57 +0530

    Create QueueProblem.cpp

---
+++ QueueProblem.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    
+    int n;cin>>n;
+    int t;cin>>t;
+    string s; 
+    cin>>s;
+    while(t--){
+    for(int i =0;i<n-1;){
+        if(s[i] == 'B' && s[i+1]=='G'){
+            swap(s[i],s[i+1]);
+            i=i+2;
+        }
+        else i++;
+        
+    }
+        
+    }
+    cout<<s;
+
+    return 0;
+}
